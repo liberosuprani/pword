5e752f0fc8358bfec4642b4f2b51e9ae7962528a
Libero Suprani <lsuprani30@gmail.com>
2024-10-23 18:51:16 +0100

    valida argumentos da linha de comando
    shell script valida os argumentos passados na linha de comando.
    
    caso um argumento opcional nao tenha sido passado, ele passa o valor default pro python

--- pword.py
+++ pword.py
@@ -6,45 +6,24 @@
 import sys
 from multiprocessing import Process
 
-
-DEFAULT_MODE = "c"
-DEFAULT_PARALLELIZATION = 1
-VALID_MODES = ["c", "l", "i"]
-
-
 # ex: ./pword -m c -p 1 -w exemplo ficheiro1.txt
 def pword(args: list):
-    mode = DEFAULT_MODE
-    parallelization = DEFAULT_PARALLELIZATION
-    word = ""
-    
-    if "-m" in args:
-        mode = args[args.index("-m")+1]
-        if mode not in VALID_MODES:
-            raise Exception("Invalid mode given. Valid arguments are: c|l|i")
-        
-    if "-p" in args:
-        parallelization = args[args.index("-p")+1]
-        
-    if "-w" not in args:
-        raise Exception("Argument -w must be provided")
-    
-    word = args[args.index("-w")+1]
-    
-    file_list = args[
-        args.index(word)+1 :       
-    ]
+    mode = args[0]
+    parallelization = int(args[1])
+    word = args[2]
+    files = args[3:]
 
-    if len(file_list) < parallelization:
-        parallelization = len(file_list)
+    if len(files) < parallelization:
+        parallelization = len(files)
         
     #TODO
-    if len(file_list) == parallelization:
+    if len(files) == parallelization:
         pass
     
     #TODO
     else:
         pass
+    
 
 
 def main(args):
