6fa4808c188690c0aba1ff171d78732b628b4130
Libero Suprani <lsuprani30@gmail.com>
2024-10-27 15:30:29 +0000

    pequenas mudanças
    comentarios e nomes de variaveis/metodos

--- pword.py
+++ pword.py
@@ -3,28 +3,31 @@
 # Aluno 2: Ravi Mughal (fc62504)
 # Aluno 3: Ricardo Avelãs (fc62257)
 
+# ex: ./pword -m c -p 1 -w exemplo ficheiro1.txt
+
 import sys
 from multiprocessing import Process
 
-# ex: ./pword -m c -p 1 -w exemplo ficheiro1.txt
+def divide_content(files: list, n_of_processes: int):
+    ...
+
+
+def assign_files_to_processes(files: list, n_of_processes: int):
+    if n_of_processes > len(files):
+        n_of_processes = len(files)
+        
+        
 def pword(args: list):
     mode = args[0]
-    parallelization = int(args[1])
+    n_of_processes = int(args[1])
     word = args[2]
     files = args[3:]
 
-    if len(files) < parallelization:
-        parallelization = len(files)
-        
-    #TODO
-    if len(files) == parallelization:
-        pass
-    
-    #TODO
+    if len(files) == 1 and n_of_processes > 1:
+        divide_content(files, n_of_processes)
     else:
-        pass
-    
-
+        assign_files_to_processes(files, n_of_processes)
+            
 
 def main(args):
     print('Programa: pword.py')
