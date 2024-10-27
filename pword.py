3d40e4654fed9b0c43dc10cc86ed83c8372a8f2b
Libero Suprani <lsuprani30@gmail.com>
2024-10-27 17:26:49 +0000

    novos metodos

--- pword.py
+++ pword.py
@@ -8,13 +8,32 @@
 import sys
 from multiprocessing import Process
 
-def divide_content(files: list, n_of_processes: int):
+def divide_content(file: list, n_of_processes: int, word: str, mode: str):
     ...
 
 
-def assign_files_to_processes(files: list, n_of_processes: int):
+def assign_files_to_processes(files: list, n_of_processes: int, word: str, mode: str):
+    p_list = []
+    
     if n_of_processes > len(files):
         n_of_processes = len(files)
+        
+    for i in range(n_of_processes):
+        p = Process(target=find_word, args=(word, files, mode))
+        p_list.append(p)
+        
+    if n_of_processes == len(files):
+        for p in p_list:
+            p.start()
+        for p in p_list:
+            p.join()
+            
+    else:
+        ...
+  
+  
+def find_word(word: str, files: list, mode):
+    ...
         
         
 def pword(args: list):
@@ -23,8 +42,8 @@ def pword(args: list):
     word = args[2]
     files = args[3:]
 
-    if len(files) == 1 and n_of_processes > 1:
-        divide_content(files, n_of_processes)
+    if len(files) == 1 and n_of_processes > 1: # 1 file and multiple processes
+        divide_content(files[0], n_of_processes)
     else:
         assign_files_to_processes(files, n_of_processes)
             
